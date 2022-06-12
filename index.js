16adac49fb784e04795ede74fccbd341279a57f9
Dipam Sen <funplanet.m4f@gmail.com>
2022-06-13 01:10:29 +0530

    make mhchem api

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const mhchem = require("mhchemparser");
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/parseTex", (req, res) => {
+  const tex = req.query.tex;
+  const result = mhchem.mhchemParser.toTex(tex, "tex");
+  res.type("text").send(result);
+});
+
+app.post("/parseTex", (req, res) => {
+  const tex = req.body.tex;
+  const result = mhchem.mhchemParser.toTex(tex, "tex");
+  res.type("text").send(result);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started on port 3000");
+});
